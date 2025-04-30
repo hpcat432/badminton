d2476a01f46fbc161152deba704c8ff98c3c3ab8
sanmao <zhangyucheng@papegames.net>
2025-04-30 11:25:16 +0800

    init

---
+++ cloudfunctions/quickstartFunctions/fetchGoodsList/index.js
@@ -0,0 +1,32 @@
+
+const cloud = require('wx-server-sdk');
+cloud.init({ env: cloud.DYNAMIC_CURRENT_ENV });
+
+const db = cloud.database();
+
+exports.main = async (event, context) => {
+  const result = await db.collection('goods')
+    .skip(0)
+    .limit(10)
+    .get();
+  return {
+    dataList: result?.data,
+  };
+};
+        
+
+// const cloud = require('wx-server-sdk');
+// cloud.init({ env: cloud.DYNAMIC_CURRENT_ENV });
+
+// const db = cloud.database();
+
+// exports.main = async (event, context) => {
+//   const result = await db.collection('goods')
+//     .skip(0)
+//     .limit(10)
+//     .get();
+//   return {
+//     dataList: result?.data,
+//   };
+// };
+        
